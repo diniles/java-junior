a6d6bc17accffdba2485257fe3399a6eb925e7a2
diniles <dzliashko@gmail.com>
2023-11-27 12:20:00 +0300

    Init: task 2

---
+++ src/hw1/task2/SemiFinishedFood.java
@@ -0,0 +1,8 @@
+package hw1.task2;
+
+/**
+ * Полуфабрикат
+ */
+public interface SemiFinishedFood extends Food {
+}
+
---
+++ src/hw1/task2/Snack.java
@@ -0,0 +1,8 @@
+package hw1.task2;
+
+/**
+ * Снек (легкая закуска)
+ */
+public interface Snack extends Food {
+}
+
