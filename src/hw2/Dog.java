594b8450783ca937ad1e00e3ddde3fa019c806e2
Dzmitry Liashko <dzliashko@gmail.com>
2023-12-05 18:22:22 +0300

    Feat: seminar 2 Dog class

---
+++ src/hw2/Dog.java
@@ -0,0 +1,21 @@
+package hw2;
+
+public class Dog extends Animal {
+    private boolean hasTale;
+
+    public Dog(String name, int age, boolean hasTale) {
+        super(name, age);
+        this.hasTale = hasTale;
+    }
+
+    public void makeBark() {
+        System.out.printf("%s is barking...", getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" + "name='" + getName() + '\'' +
+                ", age=" + getAge() + ", hasTale=" + hasTale +
+                '}';
+    }
+}
