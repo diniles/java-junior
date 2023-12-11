e0ed5187dd9317fe3131cb976494dd434cc31f75
Dzmitry Liashko <dzliashko@gmail.com>
2023-12-11 21:14:04 +0300

    Feat: seminar 3 class Student

--- src/hw3/Student.java
+++ src/hw3/Student.java
@@ -3,6 +3,8 @@ package hw3;
 import java.io.Serializable;
 
 public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String name;
     private int age;
     private transient double GPA;
